9dc1ddb10520d046521ffec7695ad4629d4a3313
owl-app <owl@pawelkeska.eu>
2023-09-06 09:51:11 +0000

    Psalm fixes

--- Doctrine/ORM/EquipmentAttributeValueRepository.php
+++ Doctrine/ORM/EquipmentAttributeValueRepository.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace Owl\Bundle\EquipmentBundle\Doctrine\ORM;
 
+use Owl\Component\Equipment\Model\EquipmentAttributeValueInterface;
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 use Owl\Component\Equipment\Repository\EquipmentAttributeValueRepositoryInterface;
 
+/**
+ * @template T of EquipmentAttributeValueInterface
+ *
+ * @implements EquipmentAttributeValueRepositoryInterface<T>
+ */
 class EquipmentAttributeValueRepository extends EntityRepository implements EquipmentAttributeValueRepositoryInterface
 {
     public function findByJsonChoiceKey(string $choiceKey): array
